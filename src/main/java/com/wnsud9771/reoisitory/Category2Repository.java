c64d815beef29039f58eb06b25e7defe0a3d3414
xlzbthxyjkoo <happyyj01@naver.com>
2024-10-23 11:13:27 +0900

    feat: createCampaign

--- src/main/java/com/wnsud9771/reoisitory/Category2Repository.java
+++ src/main/java/com/wnsud9771/reoisitory/Category2Repository.java
@@ -1,9 +1,11 @@
 package com.wnsud9771.reoisitory;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wnsud9771.entity.Category2;
 
-public interface Category2Repository extends JpaRepository<Category2, Long>{
-	
+public interface Category2Repository extends JpaRepository<Category2, Long> {
+    Optional<Category2> findByCategory2(String category2);
 }
