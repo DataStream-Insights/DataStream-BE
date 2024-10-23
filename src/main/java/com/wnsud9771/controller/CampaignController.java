3c629aa81cd310e7c7e0b6e9b723a669e46d5884
jun9771 <jun97718@gmail.com>
2024-10-23 15:32:42 +0900

    error-dbnull

--- src/main/java/com/wnsud9771/controller/CampaignController.java
+++ src/main/java/com/wnsud9771/controller/CampaignController.java
@@ -1,6 +1,5 @@
 package com.wnsud9771.controller;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,37 +9,42 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.wnsud9771.dto.CampaignDTO;
 import com.wnsud9771.service.CampaignService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/api/campaigns")
+@Slf4j
 public class CampaignController {
-	
-	 @Autowired
-	 private CampaignService campaignService;
 
-	    @GetMapping
-	    public List<CampaignDTO> getAllCampaigns() {
-	        return campaignService.getAllCampaigns();
-	    }
+	@Autowired
+	private CampaignService campaignService;
 
-	    
-	    
-	    @PostMapping
-	    public ResponseEntity<CampaignDTO> createCampaign(@RequestBody CampaignDTO campaignDTO) {
-	        try {
-	            CampaignDTO created = campaignService.createCampaign(campaignDTO);
-	            return ResponseEntity.ok(created);
-	        } catch (Exception e) {
-	            return ResponseEntity.badRequest().build();
-	        }
-	    }
-	    
+	@GetMapping
+	public List<CampaignDTO> getAllCampaigns() {
+		return campaignService.getAllCampaigns();
+	}
+
+	@PostMapping("/add")
+	public RedirectView createCampaign(@RequestBody CampaignDTO campaignDTO) {
+		try {
+			// campaignDTO.setNo(null);
+			log.info("Received DTO: {}", campaignDTO);
+			CampaignDTO created = campaignService.createCampaign(campaignDTO);
+			return new RedirectView("/campaigns");
+		} catch (Exception e) {
+			log.info("error message", e.getMessage());
+			return new RedirectView("/add");
+		}
+	}
+
 //	    @PostMapping
 //	    public Campaign createCampaign(@RequestBody Campaign campaign) {
 //	        return campaignService.saveCampaign(campaign);
 //	    }
-	    
+
 }
--- src/main/java/com/wnsud9771/reoisitory/AuthorRepository.java
+++ src/main/java/com/wnsud9771/reoisitory/AuthorRepository.java
@@ -1,9 +1,11 @@
 package com.wnsud9771.reoisitory;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wnsud9771.entity.Author;
 
 public interface AuthorRepository extends JpaRepository<Author, Long>{
-
+	 Optional<Author> findById(Long id);
 }
