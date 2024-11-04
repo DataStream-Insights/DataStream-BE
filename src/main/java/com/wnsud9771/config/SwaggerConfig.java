5c1bb22e96881a26910eaedbb3ed5e6d1fd6473d
xlzbthxyjkoo <happyyj01@naver.com>
2024-11-04 20:10:05 +0900

    feat: swagger

--- src/main/java/com/wnsud9771/config/SwaggerConfig.java
+++ src/main/java/com/wnsud9771/config/SwaggerConfig.java
@@ -2,33 +2,19 @@ package com.wnsud9771.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.wnsud9771.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-    
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("DataStream API Documentation")
-                .description("DataStream 프로젝트의 API 문서")
-                .version("1.0")
-                .build();
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("DataStream API Documentation")
+                        .description("DataStream 프로젝트의 API 문서")
+                        .version("1.0"));
     }
 }
