f542053544bdedef0043fcf79be74f0521ab4f82
xlzbthxyjkoo <happyyj01@naver.com>
2024-11-07 15:37:00 +0900

    filter options

--- src/main/java/com/wnsud9771/entity/FIlterentity/Operation.java
+++ src/main/java/com/wnsud9771/entity/FIlterentity/Operation.java
@@ -1,19 +1,9 @@
 package com.wnsud9771.entity.FIlterentity;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.wnsud9771.entity.Authorentity.Author;
-import com.wnsud9771.entity.Formatentity.FormatSet;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,13 +11,17 @@ import lombok.Setter;
 @Setter
 @Entity
 public class Operation {
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-	private String operation;
-	
-	//filter set 으로 가야함
-	@OneToMany(mappedBy = "operation", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<FilterSet> filterSets = new ArrayList<>();
+    private String operation;    // VARCHAR(255)
+//	@Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//	private String operation;
+//	
+//	//filter set 으로 가야함
+//	@OneToMany(mappedBy = "operation", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+//    private List<FilterSet> filterSets = new ArrayList<>();
 }
---
+++ src/main/java/com/wnsud9771/reoisitory/filter/OperationRepository.java
@@ -0,0 +1,7 @@
+package com.wnsud9771.reoisitory.filter;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.wnsud9771.entity.FIlterentity.Operation;
+
+public interface OperationRepository extends JpaRepository<Operation, Long> {
+}
