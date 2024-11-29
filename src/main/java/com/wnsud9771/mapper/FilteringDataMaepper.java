00340476fa945312b5f6db7444b31ada4fb2c8c5
jun9771 <jun97718@gmail.com>
2024-11-29 14:21:04 +0900

    fix-dash-top5

--- src/main/java/com/wnsud9771/mapper/FilteringDataMaepper.java
+++ src/main/java/com/wnsud9771/mapper/FilteringDataMaepper.java
@@ -13,11 +13,13 @@ import com.wnsud9771.dto.dashboard.visits.VisitDayAndCountDTO;
 
 @Mapper
 public interface FilteringDataMaepper {
+//	@Select("SELECT data, COUNT(*) as count " + "FROM filtering_data " + "WHERE pipelines_id = #{pipelineId} "
+//			+ "GROUP BY data " + "ORDER BY count DESC")
+//	List<DataAndCountDTO> findDataCountByPipelineId(Long pipelineId);
+	
 	@Select("SELECT data, COUNT(*) as count " + "FROM filtering_data " + "WHERE pipelines_id = #{pipelineId} "
-			+ "GROUP BY data " + "ORDER BY count DESC")
+			+ "GROUP BY data " + "ORDER BY count DESC " + "LIMIT 5")
 	List<DataAndCountDTO> findDataCountByPipelineId(Long pipelineId);
-	
-	
 
 	// 날짜별 방문 수 countByDate
 	@Select("SELECT " + "DATE_FORMAT(timestamp, '%Y-%m-%d') as date, " + "COUNT(*) as count " + "FROM filtering_data "
@@ -29,16 +31,14 @@ public interface FilteringDataMaepper {
 			+ "FROM filtering_data " + "WHERE pipelines_id = #{pipelineId} " + "AND DATE(timestamp) = #{date} "
 			+ "GROUP BY DATE_FORMAT(timestamp, '%H') " + "ORDER BY timeRange")
 	List<TimeRangeAndCountDTO> countByTimeRangeForDate(Long pipelineId, String date);
-	
-	// 날짜 상관없이 모든 날짜 시간대별 getTimeRangeCount
-	@Select("SELECT DATE_FORMAT(timestamp, '%H:00-%H:59') as timeRange, COUNT(*) as count " +
-		       "FROM filtering_data " +
-		       "WHERE pipelines_id = #{pipelineId} " +
-		       "GROUP BY DATE_FORMAT(timestamp, '%H') " +
-		       "ORDER BY DATE_FORMAT(timestamp, '%H')")
-		List<OnlyTimeDTO> getTimeRangeCount(Long pipelineId);
 
-	//요일별( 수요일,목요일, 금요일) countByDay
+	// 날짜 상관없이 모든 날짜 시간대별 getTimeRangeCount
+	@Select("SELECT DATE_FORMAT(timestamp, '%H:00-%H:59') as timeRange, COUNT(*) as count " + "FROM filtering_data "
+			+ "WHERE pipelines_id = #{pipelineId} " + "GROUP BY DATE_FORMAT(timestamp, '%H') "
+			+ "ORDER BY DATE_FORMAT(timestamp, '%H')")
+	List<OnlyTimeDTO> getTimeRangeCount(Long pipelineId);
+
+	// 요일별( 수요일,목요일, 금요일) countByDay
 	@Select("SELECT DATE_FORMAT(timestamp, '%W') as day, COUNT(*) as count " + "FROM filtering_data "
 			+ "WHERE pipelines_id = #{pipelineId} " + "GROUP BY DATE_FORMAT(timestamp, '%W') "
 			+ "ORDER BY FIELD(day, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')")
