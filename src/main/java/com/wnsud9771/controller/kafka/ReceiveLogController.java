af5b252ffa54a907f1235b8c26e80883cde2845c
jun9771 <jun97718@gmail.com>
2024-12-04 17:06:23 +0900

    fix-addf

--- src/main/java/com/wnsud9771/controller/kafka/ReceiveLogController.java
+++ src/main/java/com/wnsud9771/controller/kafka/ReceiveLogController.java
@@ -1,29 +1,29 @@
-package com.wnsud9771.controller.kafka;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.wnsud9771.dto.receivelog.LogDTO;
-import com.wnsud9771.service.format.ReceiveLogAndParseTitleService;
-
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@RestController
-@RequestMapping("/logs")
-@RequiredArgsConstructor
-@Slf4j
-public class ReceiveLogController {
-	private final ReceiveLogAndParseTitleService receiveLogAndParseTitleService;
-
-	@Operation(summary = "첫 쇼핑몰 로그가 들어오는 경로", description = "맨처음 쇼핑몰에서 spring-kafka로 컨슈밍되어 들어온 로그를 그대로 받아와서 title추출후 db에 title과 로그를 같이 저장")
-	@PostMapping("/receive")
-	public ResponseEntity<LogDTO> receiveUserFromFirstService(@RequestBody LogDTO logDTO) {
-		log.info("log data {}:", logDTO.getLog_data());
-		return ResponseEntity.ok(receiveLogAndParseTitleService.receiveLogData(logDTO));
-	}
-}
+//package com.wnsud9771.controller.kafka;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.wnsud9771.dto.receivelog.LogDTO;
+//import com.wnsud9771.service.format.ReceiveLogAndParseTitleService;
+//
+//import io.swagger.v3.oas.annotations.Operation;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//
+//@RestController
+//@RequestMapping("/logs")
+//@RequiredArgsConstructor
+//@Slf4j
+//public class ReceiveLogController {
+//	private final ReceiveLogAndParseTitleService receiveLogAndParseTitleService;
+//
+//	@Operation(summary = "첫 쇼핑몰 로그가 들어오는 경로", description = "맨처음 쇼핑몰에서 spring-kafka로 컨슈밍되어 들어온 로그를 그대로 받아와서 title추출후 db에 title과 로그를 같이 저장")
+//	@PostMapping("/receive")
+//	public ResponseEntity<LogDTO> receiveUserFromFirstService(@RequestBody LogDTO logDTO) {
+//		log.info("log data {}:", logDTO.getLog_data());
+//		return ResponseEntity.ok(receiveLogAndParseTitleService.receiveLogData(logDTO));
+//	}
+//}
